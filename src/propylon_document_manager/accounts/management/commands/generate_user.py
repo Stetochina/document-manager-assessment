5f4434417f74aa6fbe638bc6684b3dddc7da1105
Bogdan Kalinic <bogdankalinic@Bogdans-MacBook-Pro.local>
2025-05-03 11:04:55 +0200

    created accounts app and modified file_versions models

---
+++ src/propylon_document_manager/accounts/management/commands/generate_user.py
@@ -0,0 +1,4 @@
+from django.core.management.base import BaseCommand
+from propylon_document_manager.accounts.models import User
+from propylon_document_manager.exceptions.ecxeptions import UserAlreadyExists
+
